9faa79dad45ef7d477a43004d0866fa174b7f5c3
Sebastian Schmied <sebastian.schmied@1und1.de>
2018-02-15 10:17:30 +0100

    permalink

--- flights/showmap.js
+++ flights/showmap.js
@@ -7,6 +7,7 @@ var mapcontainer = document.getElementById('maps-container');
 var src = kmzbaseurl+ "flightbook/530/2018-01-01_11_08_56.igc.kmz"; //maps api needs a dummy kmz for initializing
 
 function initialize() {
+    var hash = location.hash;
     mapelem = document.getElementById("google-maps-view-of-track");
     map = new google.maps.Map(mapelem, {
         center: new google.maps.LatLng(lat, long),
@@ -19,15 +20,28 @@ function initialize() {
         url: src, //dummy
         map: map
     });
+    
+    if (hash.match(/^#flight\-[0-9]*$/g))
+        selectFlight($(hash));
+    
+    
+}
+
+function selectFlight(elem) {
+    
+    var activeclass = "tr-active";
+    $('.tr-showmap').not(elem).removeClass(activeclass);
+    elem.addClass(activeclass);
+    kmlurl = kmzbaseurl + "/" + elem.data("kmz");
+    if (kmlurl.includes("/blob/") && kmlurl.includes("github.com")) //do we use github binaries?
+            kmlurl += "?raw=true";
+    layer.setUrl(kmlurl);
+    layer.setMap(map);
+    elem[0].scrollIntoView(true);
+    
 }
 $(document).ready(function() {
     $('.tr-showmap').click(function() {
-        $('.tr-showmap').not(this).removeClass("tr-active");
-        $(this).addClass("tr-active");
-        kmlurl = kmzbaseurl + "/" + $(this).data("kmz");
-        if (kmlurl.includes("/blob/") && kmlurl.includes("github.com")) //do we use github binaries?
-            kmlurl += "?raw=true";
-        layer.setUrl(kmlurl);
-        layer.setMap(map);
+        selectFlight($(this));
     });
 });
